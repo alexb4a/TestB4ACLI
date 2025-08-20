9013e8a254b576184b4435f851debee809f80e25
alexb4a <alex@back4app.com>
2025-08-20 11:29:49 -0300

    Fixed eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,29 @@
-import fs from 'fs';
+import eslintPluginImport from 'eslint-plugin-import';
 
-export default {
-  root: true,
-  env: {
-    es2021: true,
-    node: true,
-    jest: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  rules: {
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': 'off',
-    eqeqeq: ['error', 'always'],
-    curly: ['error', 'all'],
-    semi: ['error', 'always'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    indent: ['error', 2],
-    'import/order': [
-      'error',
-      {
-        groups: [['builtin', 'external', 'internal']],
-        'newlines-between': 'always'
-      }
-    ]
+export default [
+  {
+    files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module'
+    },
+    plugins: {
+      import: eslintPluginImport
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external', 'internal']],
+          'newlines-between': 'always'
+        }
+      ]
+    }
   }
-};
+];
